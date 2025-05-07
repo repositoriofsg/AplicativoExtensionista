cfa0a37b21ca741eb143498724174a461d7dca17
Marcio Barcellos <marcbrcx@gmail.com>
2025-05-07 20:15:33 -0300

    Teste SonarQube

--- app/src/main/java/com/aulaquinta/aplicativoextensionista/activity/MainActivity.java
+++ app/src/main/java/com/aulaquinta/aplicativoextensionista/activity/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
 
+    //teste sonarqube
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
